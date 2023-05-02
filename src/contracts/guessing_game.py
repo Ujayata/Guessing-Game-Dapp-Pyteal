45139be49c719494034ecc398b98841ca32ed833
Ujayata <62179207+Ujayata@users.noreply.github.com>
2023-05-02 23:36:14 +0100

    Update guessing_game.py

--- src/contracts/guessing_game.py
+++ src/contracts/guessing_game.py
@@ -1,207 +1,91 @@
-from pyteal import *
-from pyteal.ast.bytes import Bytes
+// This is a modified version of the guessing_game.teal smart contract with suggested solutions to issues.
 
+// Declare application ID
+// Note: This should be changed to the actual ID when deploying the contract
+txn ApplicationID
+int 0
+==
+bnz app_init
 
-router = Router(
-    "Guessing Game",
-    BareCallActions(
-        no_op=OnCompleteAction.create_only(Approve()),
-        opt_in=OnCompleteAction.call_only(Approve())
-    )
-)
+// Define application creation function
+app_init:
+// Define global schema
+byte "ApprovalProgram"
+byte "Int"
+byte "Local"
+byte "Int"
+byte "Global"
+byte "Int"
+int 1
+int 1
+int 1
+byte "ClearStateProgram"
+byte "OnCompletion"
+byte "NoOp"
+int 0
+byte "Accounts"
+int 1
+int 1
+// Define approval program
+// Solution to issue 1: Initialize guessed variable with -1 to indicate uninitialized state
+int -1
+txn ApplicationArgs 0
+byte "guess"
+==
+bnz handle_guess
+// Solution to issue 2: Remove unnecessary condition checks in the approval program
+int 1
+return
 
-player1 = Bytes("player1")
-player2 = Bytes("player2")
-wager =  Bytes("wager")
-l_p = Bytes("l_p1")
-seed = Bytes("p1Rand")
-addresses = []
+handle_guess:
+// Solution to issue 1: Check that guessed value is greater than or equal to 0
+int 0
+txn ApplicationArgs 1
+>=
+bnz valid_guess
+// Solution to issue 3: Revert transaction if guessed value is negative
+err
+valid_guess:
+// Load global value
+// Solution to issue 1: Only load the guessed value if it has been initialized
+global RoundGuess
+int -1
+==
+bnz set_guess
+// Solution to issue 4: Set the guessed value to the user's input
+txn ApplicationArgs 1
+global StoreRoundGuess
+// Solution to issue 5: Increment the round count
+global RoundCount
+int 1
++
+global StoreRoundCount
+// Solution to issue 6: Check if the user's input is the correct answer
+txn ApplicationArgs 1
+global Answer
+==
+bnz correct_guess
+// Solution to issue 7: Set the return value to 0 if the guess is incorrect
+int 0
+return
 
+set_guess:
+// Solution to issue 1: Only set the guessed value if it has been initialized
+global RoundGuess
+int -1
+==
+bnz store_guess
+// Solution to issue 8: Revert transaction if guessed value has already been set for the round
+err
+store_guess:
+// Store guessed value in global state
+txn ApplicationArgs 1
+global StoreRoundGuess
+// Return 1 to indicate successful guess
+int 1
+return
 
-@router.method
-def create_challenge(rand: abi.String, payment: abi.PaymentTransaction):
-        return Seq(
-            Assert(
-                And(
-                    
-                    payment.get().type_enum() == TxnType.Payment,
-                    payment.get().receiver() == Global.current_application_address(),
-                    payment.get().close_remainder_to() == Global.zero_address(),
-                    App.optedIn(payment.get().sender(), Global.current_application_id()),
-                    
-                )
-            ),
-            App.globalPut(player1, payment.get().sender()),
-            Assert(
-                payment.get().sender() == App.globalGet(Bytes("player1")),
-            ),
-            App.globalPut(wager, payment.get().amount()),
-            App.globalPut(seed, rand.get()),
-            
-
-            Approve(),
-        )
-@router.method
-def show_seed(*, output: abi.Uint64):
-    return output.set(App.globalGet(seed))
-
-
-@router.method
-def accept_challenge( payment: abi.PaymentTransaction):
-        return Seq(
-            Assert(
-                And(
-                    payment.get().type_enum()  == TxnType.Payment,
-                    payment.get().receiver() == Global.current_application_address(),
-                    payment.get().close_remainder_to() == Global.zero_address(),
-                    App.globalGet(wager) == payment.get().amount(),
-                    App.optedIn(payment.get().sender(), Global.current_application_id()),
-
-                )
-            ),
-            App.globalPut(player2, payment.get().sender()),
-           
-            Approve(),
-        )
-
-@router.method
-def play_value(p: abi.String):
-        return Seq(
-            Assert(
-                And(
-                    App.optedIn(Txn.sender(), Global.current_application_id()),)
-                ),
-                If(Txn.sender() == App.globalGet(Bytes("player1")))
-                .Then(
-                    App.localPut(Txn.sender(), l_p, p.get()),
-                  
-                )
-                .ElseIf((Txn.sender() == App.globalGet(Bytes("player2"))))
-                .Then(
-                    App.localPut(Txn.sender(), l_p, p.get()),
-                   
-                    )
-                ,
-               
-                Approve(),
-            )
-
-
-@router.method
-def show_play(*,output: abi.String):
-   return output.set(Base64Decode.std(App.localGet(App.globalGet(Bytes("player1")), l_p)))
-    
-
-
-@Subroutine(TealType.uint64)
-def winner(play1: Expr, play2: Expr):
-        #v = App.globalGet(seed),
-        return Seq(
-            Return(Cond(
-                [And(Base64Decode.std(play1) == Base64Decode.std(App.globalGet(seed)),Base64Decode.std(play2) == Base64Decode.std(App.globalGet(seed)) ), Int(3)],
-                [And(Base64Decode.std(play1) == Base64Decode.std(App.globalGet(seed)), Base64Decode.std(play2) != Base64Decode.std(App.globalGet(seed))), Int(1)],
-                [And(Base64Decode.std(play1) != Base64Decode.std(App.globalGet(seed)), Base64Decode.std(play2) == Base64Decode.std(App.globalGet(seed)) ), Int(2)],
-                ))
-            
-            )
-
-@Subroutine(TealType.none)
-def send_rewards(account : Expr , amount: Expr):
-    return Seq(
-        InnerTxnBuilder.Begin(),
-        InnerTxnBuilder.SetFields(
-            { TxnField.type_enum : TxnType.Payment,
-                TxnField.receiver : account,
-                TxnField.amount : amount
-            }
-        ),
-        InnerTxnBuilder.Submit()
-    )
-
- 
-
-@router.method
-def reveal(account1:abi.Account, account2: abi.Account, *, output: abi.Uint64 ):
-    Assert(
-        Txn.sender() == App.globalGet(Bytes("player1")),
-    ),
-  
-    var = winner(App.localGet(account1.address(), l_p), App.localGet(account2.address(), l_p))
-
-    p = "Player 1 Won"
-    p2 = "Player 2 Won"
-    p3 ="Draw"
-    p4 = "No one won"
-    return Seq(
-        
-        If(var == Int(3))
-        .Then(
-            Seq(
-            send_rewards(account1.address(), App.globalGet(wager) - (App.globalGet(wager)*Int(15)/Int(100))),
-            send_rewards(account2.address(), App.globalGet(wager) - (App.globalGet(wager)*Int(15)/Int(100))),
-            reset_all(account1.address(), account2.address())
-          
-            ))
-        .ElseIf(var == Int(2))
-        .Then(
-            Seq(
-                send_rewards(account2.address(), App.globalGet(wager) * Int(2) - (App.globalGet(wager)*Int(15)/Int(100))),
-                reset_all(account1.address(), account2.address())
-            )
-        )
-        .ElseIf(var == Int(1))
-        .Then(
-            Seq(
-                send_rewards(account1.address(), App.globalGet(wager) * Int(2) - (App.globalGet(wager)*Int(15)/Int(100))),
-                reset_all(account1.address(), account2.address())
-            )
-        )
-        .Else(
-            reset_all(account1.address(), account2.address())
-        ),
-        Approve(),
-    )
-
-@router.method
-def show_winner(account1: abi.Account, account2: abi.Account,*, output: abi.Uint64):
-    return output.set(winner(App.localGet(account1.address(), l_p), App.localGet(account2.address(), l_p))
-)
-
-
-@router.method
-def getPlayer1(*, output: abi.Address):
-    return Seq(
-        output.set(App.globalGet(Bytes("player1"))),
-    )
-@router.method
-def getPlayer2(*, output: abi.Address):
-    return Seq(
-        output.set(App.globalGet(Bytes("player2"))),
-    )
-@Subroutine(TealType.none)
-def reset_all(acc1 : Expr , acc2 : Expr):
-    return Seq(App.localPut(acc1 , l_p , Bytes("")),
-                App.localPut(acc2 , l_p , Bytes("")),
-                App.globalPut(seed, Int(0)),
-                App.globalPut(wager , Int(0)),
-                )
-
-if __name__ == "__main__":
-    import os
-    import json
-
-    path = os.path.dirname(os.path.abspath(__file__))
-    approval, clear, contract = router.compile_program(version=8)
-
-    # Dump out the contract as json that can be read in by any of the SDKs
-    with open(os.path.join(path, "artifacts/contract.json"), "w") as f:
-        f.write(json.dumps(contract.dictify(), indent=2))
-
-    # Write out the approval and clear programs
-    with open(os.path.join(path, "artifacts/approval.teal"), "w") as f:
-        f.write(approval)
-
-    with open(os.path.join(path, "artifacts/clear.teal"), "w") as f:
-        f.write(clear)
-
-       
+correct_guess:
+// Solution to issue 9: Set the return value to 1 if the guess is correct
+int 1
+return
